505ea903ef35153d87a8eafd90bd93c7b26a1336
arghac14 <chakrabortyarghadip5@gmail.com>
2019-07-14 21:08:55 +0530

    Adding redux and reducers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css'
 import * as serviceWorker from './serviceWorker';
+import { createStore} from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store=createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,6 @@
+const initState={}
+
+const projctReducer=(state=initState,action)=>{
+    return state;
+}
+export default projctReducer
