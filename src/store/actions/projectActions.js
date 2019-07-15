e3950d7194a2cab0f9ac634b340604c1c00ba360
arghac14 <chakrabortyarghadip5@gmail.com>
2019-07-15 22:46:57 +0530

    Adding data to firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,20 @@
 export const createProject=(project)=>{
     return(dispatch, getState,{getFirebase,getFirestore})=>{
         //async call to database
-        dispatch({type:'CREATE_PROJECT', project})
+        const firestore=getFirestore()
+        firestore.collection('projects').add({
+            ... project,
+            firstname: 'Argha',
+            lastname: 'C',
+            authorId: 123,
+            createdAt : new Date()
+        }).then(()=>{
+            dispatch({type:'CREATE_PROJECT', project})
+        }).catch((err)=>{
+            dispatch({type:'CREATE_PROJECT_ERROR', err})
+        })
+
+
+        
     }
 }
