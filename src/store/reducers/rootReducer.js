82e9d75fc131ce0a9cb2f07d9ca632bc58a7ac21
arghac14 <chakrabortyarghadip5@gmail.com>
2019-07-16 14:04:35 +0530

    Syncing data with Firestore

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,10 +1,11 @@
 import authReducer from './authReducer'
 import projectReducer from './projectReducer'
 import { combineReducers} from 'redux'
-
+import {firestoreReducer} from 'redux-firestore'
 const rootReducer=combineReducers({
     auth: authReducer,
-    project: projectReducer
+    project: projectReducer,
+    firestore: firestoreReducer
 })
 
 export default rootReducer;
