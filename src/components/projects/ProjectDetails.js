4273175e0e6b12734729c312b1da7a8f7f7853c5
arghac14 <chakrabortyarghadip5@gmail.com>
2019-07-16 14:24:17 +0530

    Project Details Data

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,22 +1,47 @@
-import React  from 'react';
+import React from 'react'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
-const ProjectDetails=(props)=>{
-    const id=props.match.params.id;
-    return(        
-        <div className="container">
-        <br></br><br></br>
-            <div className="card z-depth-4">
-                <div className="card-content">
-                    <span className="card-title black-text"><b>Title {id}</b></span>
-                    <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.</p>
-                </div>
-                <div className="card-action grey lighten-4 grey-text">
-                    <div>Posted by </div>
-                    <div>Date</div>
-                </div>
-            </div>
+const ProjectDetails = (props) => {
+  const { project } = props;
+  if (project) {
+    return (
+      <div className="container section project-details">
+        <div className="card z-depth-0">
+          <div className="card-content">
+            <span className="card-title">{project.title}</span>
+            <p>{project.content}</p>
+          </div>
+          <div className="card-action grey lighten-4 grey-text">
+            <div>Posted by {project.firstname} {project.lastname}</div>
+            <div>2nd September, 2am</div>
+          </div>
         </div>
+      </div>
     )
+  } else {
+    return (
+      <div className="container center">
+        <p>Loading project...</p>
+      </div>
+    )
+  }
 }
 
-export default ProjectDetails;
+const mapStateToProps = (state, ownProps) => {
+  // console.log(state);
+  const id = ownProps.match.params.id;
+  const projects = state.firestore.data.projects;
+  const project = projects ? projects[id] : null
+  return {
+    project: project
+  }
+}
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{
+    collection: 'projects'
+  }])
+)(ProjectDetails)
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,7 +5,7 @@ const ProjectSummary=({project})=>{
         <div className="card">
         <div className="card-content grey-text text-darken-2">
             <span className="card-title">{project.title}</span>
-            <p>Posted by </p>
+            <p>Posted by {project.firstname} {project.lastname} </p>
             <p className="grey-text">21st October, 1997</p>
         </div>
         </div>
